e09131565576b25aa91eb1795f6da49e46649b7c
Hiro Asari <asari.ruby@gmail.com>
2014-05-06 17:18:07 -0400

    Add Travis::Build::Shell::*::Linux classes
    If.create needs this

--- lib/travis/build/shell/node/linux.rb
+++ lib/travis/build/shell/node/linux.rb
@@ -1,2 +1,42 @@
-# this file is left intentionally blank
-# Linux is the default, so there is nothing to do here.
+module Travis
+  module Build
+    module Shell
+      class Node
+        class Linux < Node; end
+      end
+
+      class Cmd < Node
+        class Linux < Cmd; end
+      end
+
+      class Group < Node
+        class Linux < Group
+        end
+      end
+
+      class Script < Group
+        class Linux < Script; end
+      end
+
+      class Block < Group
+        class Linux < Block; end
+      end
+
+      class Conditional < Block
+        class Linux < Conditional; end
+      end
+
+      class If < Conditional
+        class Linux < If; end
+      end
+
+      class Elif < Conditional
+        class Linux < Elif; end
+      end
+
+      class Else < Block
+        class Linux < Else; end
+      end
+    end
+  end
+end
