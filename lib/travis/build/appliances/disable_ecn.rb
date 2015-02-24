5661c6a90e434324d8e990a56e6c5da6d99700e6
Hiro Asari <asari.ruby@gmail.com>
2015-02-24 14:35:39 -0500

    Actually disable ECN
    and write a message

--- lib/travis/build/appliances/disable_ecn.rb
+++ lib/travis/build/appliances/disable_ecn.rb
@@ -4,9 +4,10 @@ module Travis
   module Build
     module Appliances
       class DisableEcn < Base
-        CMD = 'echo 2 | sudo tee /proc/sys/net/ipv4/tcp_ecn'
+        CMD = 'echo 0 | sudo tee /proc/sys/net/ipv4/tcp_ecn'
 
         def apply
+          sh.echo "Disabling ECN", ansi: yellow
           sh.cmd CMD
         end
 
